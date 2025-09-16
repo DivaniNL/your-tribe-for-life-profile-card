d267491a4737ed397fa9ae696c5d035c8aaeb771
DivaniNL <dylanvannierop@gmail.com>
2025-09-16 14:25:13 +0200

    #11 opleiding pagina gemaakt

---
+++ profile-card/src/routes/education/+page.svelte
@@ -0,0 +1,144 @@
+<script>
+    let { data } = $props();
+    let opleidingen = data.opleidingen;
+</script>
+
+<div id="container">
+    <section class="profile-card">
+        <picture>
+            <source
+                srcset="https://fdnd.directus.app/assets/{data.person.mugshot
+                    .id}?width=600&height=600&format=avif"
+                type="image/avif"
+            />
+            <source
+                srcset="https://fdnd.directus.app/assets/{data.person.mugshot
+                    .id}?width=600&height=600&format=webp"
+                type="image/webp"
+            />
+            <img
+                src="https://fdnd.directus.app/assets/{data.person.mugshot
+                    .id}?width=600&height=600"
+                alt="test"
+                width="600"
+                height="600"
+            />
+        </picture>
+        <div class="content-wrapper">
+            <h1 class="title-l">Opleidingen</h1>
+            <p class="text-l">Hieronder mijn Opleidingen</p>
+        </div>
+
+        <table class="table-primary table-opleidingen">
+            <thead>
+                <tr>
+                    <th>Periode</th>
+                    <th>Studie</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {#each opleidingen as opleiding}
+                    {#if opleiding.omschrijving}
+                        <tr>
+                            {#if opleiding.periode}
+                                <td>{opleiding.periode}</td>
+                            {:else}
+                                <td>Onbekende periode</td>
+                            {/if}
+
+                            <td>{opleiding.omschrijving}</td>
+                        </tr>
+                    {/if}
+                {/each}
+            </tbody>
+        </table>
+    </section>
+</div>
+
+<style>
+    .profile-card{
+        padding-left: 16px;
+        padding-right: 16px;
+    }
+    #container {
+        max-width: 1024px;
+    }
+    section.hobbies {
+        margin-bottom: 100px;
+    }
+    table {
+        border-collapse: separate;
+        border-spacing: 0;
+    }
+
+    table.table-primary {
+        border-radius: 27px;
+        overflow: hidden;
+        margin-top: 10px;
+        border: 3px solid white;
+    }
+
+    table.table-primary th,
+    table.table-primary td {
+        padding: 10px;
+        text-align: left;
+    }
+
+    table.table-primary th a,
+    table.table-primary td a {
+        color: #2c1fb8;
+        color: var(--color-primary);
+        font-weight: bold;
+        transition: 0.2s ease;
+    }
+
+    table.table-primary th a:hover,
+    table.table-primary td a:hover {
+        color: #fff;
+        color: var(--color-white);
+    }
+
+    table.table-primary thead tr th {
+        background-color: #2c1fb8;
+        background-color: var(--color-primary);
+        color: #fff;
+        color: var(--color-white);
+        border-bottom: 1px solid #2c1fb8;
+        border-bottom: 1px solid var(--color-primary);
+    }
+
+    table.table-primary tbody tr:nth-child(even) td {
+        background-color: #2c1fb8;
+        background-color: var(--color-primary);
+        color: #fff;
+        color: var(--color-white);
+        border: 1px solid #fff;
+        border: 1px solid var(--color-white);
+        font-weight: bold;
+    }
+
+    table.table-primary tbody tr:nth-child(odd) td {
+        background-color: #fff;
+        background-color: var(--color-white);
+        border: 1px solid #2c1fb8;
+        border: 1px solid var(--color-primary);
+        color: #2c1fb8;
+        color: var(--color-primary);
+        font-weight: bold;
+    }
+
+
+    table.table-primary tbody tr:last-child td:first-child {
+        border-bottom-left-radius: 24px;
+    }
+
+    table.table-primary tbody tr:last-child td:last-child {
+        border-bottom-right-radius: 24px;
+    }
+
+    table.table-primary tbody tr td {
+        font-size: 12px;
+    }
+
+</style>
